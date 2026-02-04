379c4461b47b98a7b34fe0af5a05421111dc8878
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-04 16:40:03 +0100

    seventh point

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -7,7 +7,7 @@ const teachers = [
   'Lewis',
   'Luca'
 ]; // NON MODIFICARE QUESTA VARIABILE
-
+/*
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
@@ -35,18 +35,20 @@ const firstTeacher = teachers.shift();
 console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-const otherTeacher = teachers.push('Vanessa')
+const otherTeacher = teachers.push('Vanessa');
 
-console.log(teachers)
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-const other_Teacher = teachers.unshift('Sarah')
+const other_Teacher = teachers.unshift('Sarah');
 
-console.log(teachers)
-
+console.log(teachers);
+*/
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
