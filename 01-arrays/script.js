33c385609a250a2bd21fa4c1550eb8c608ed9d1d
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-04 15:20:46 +0100

    fourth point

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -22,10 +22,12 @@ console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
 
-console.log(teachers)
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
